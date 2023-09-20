52ed62ebd466c598a70bba559830ea970ca70876
SmerchProsto <smerchprosto@gmail.com>
2023-09-20 15:10:46 +0400

    Server init
    init of server config

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express'); // import modules into file
+
+const PORT = 5000;
+
+const app = express(); // created object
+
+app.listen(PORT, () => console.log(`Server started on port ${5000}`))
